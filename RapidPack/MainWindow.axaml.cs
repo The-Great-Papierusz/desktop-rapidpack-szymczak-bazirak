2a3549da9ccad6b773d64b2692dc39eda2a1e997
Infinitzium <mikolaj.bazirak@cyfroweszkoly.pl>
2026-03-08 11:50:54 +0100

    Add Method FormChange
    Added a Method that triggers on certain form changes.

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace RapidPack;
 
@@ -12,27 +13,38 @@ public partial class MainWindow : Window
     public TextBox WeightStr;
     public string ExpressOption;
     public string ServiceOption;
-    
+
+    public int HeightInt;
+    public int WidthInt;
+    public int DepthInt;
+    public int WeightInt;
     public MainWindow()
     {
         InitializeComponent();
+        HeightInputBox.TextChanged += FormChange;
+        WidthInputBox.TextChanged += FormChange;
+        DepthInputBox.TextChanged += FormChange;
+        WeightInputBox.TextChanged += FormChange;
+        
         CalculatePriceButton.Click += CalculatePriceButton_Click;
         
         HeightStr = this.FindControl<TextBox>("HeightInputBox");
-        var HeightConv = int.TryParse(HeightStr.Text,out int HeightInt);
+        var HeightConv = int.TryParse(HeightStr.Text,out HeightInt);
         
         WidthStr = this.FindControl<TextBox>("WidthInputBox");
-        var WidthConv = int.TryParse(WidthStr.Text,out int WidthInt);
+        var WidthConv = int.TryParse(WidthStr.Text,out WidthInt);
         
         DepthStr = this.FindControl<TextBox>("DepthInputBox");
-        var DepthConv = int.TryParse(DepthStr.Text,out int DepthInt);
+        var DepthConv = int.TryParse(DepthStr.Text,out DepthInt);
         
         WeightStr = this.FindControl<TextBox>("WeightInputBox");
-        var WeightConv = int.TryParse(WeightStr.Text,out int WeightInt);
+        var WeightConv = int.TryParse(WeightStr.Text,out WeightInt);
         
         ExpressOption = ExpressCheckBox.IsChecked == true ? "Checked" : "Unchecked";
         
         ServiceOption = (ServiceComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
+
+        
         
     }
 
@@ -47,5 +59,14 @@ public partial class MainWindow : Window
             Console.WriteLine($"Error: {exception.Message}");
             throw;
         }
+    }
+
+    private void FormChange(object sender, RoutedEventArgs e)
+    {
+        var HeightConv = int.TryParse(HeightStr.Text,out HeightInt);
+        var WidthConv = int.TryParse(WidthStr.Text,out WidthInt);
+        var DepthConv = int.TryParse(DepthStr.Text,out DepthInt);
+        var WeightConv = int.TryParse(WeightStr.Text,out WeightInt);
+        
     }
 }
